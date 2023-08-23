619bf41d0b1bb622b7e9169113435c961514e803
CaoE <e.cao@intel.com>
2023-08-22 22:04:11 -0700

    add Half support for range, logspace, logit, median, nanmedian, kthvalue, poisson, cummax, cummin, prod, cumprod, histc, logcumsumexp, vander, cross, aten2, logaddexp, logaddexp2, hypot, and nextafter on CPU

--- test/test_mps.py
+++ test/test_mps.py
@@ -165,7 +165,10 @@ def mps_ops_grad_modifier(ops):
         '__rpow__': [torch.float32],
 
         # See https://github.com/pytorch/pytorch/issues/106112 for more information
-        'cumprod': [torch.float32],
+        'cumprod': [torch.float32, torch.float16],
+
+        # See https://github.com/pytorch/pytorch/issues/109166 for more information
+        'masked.cumprod': [torch.float16],
     }
 
     XPASSLIST_GRAD = {
@@ -378,7 +381,7 @@ def mps_ops_modifier(ops):
         'square': [torch.bool, torch.int16, torch.int32, torch.int64, torch.uint8, torch.int8],
 
         # cpu not giving nan for x/0.0
-        'atan2': [torch.bool, torch.float16, torch.int16, torch.int32, torch.int64, torch.uint8, torch.int8],
+        'atan2': [torch.bool, torch.int16, torch.int32, torch.int64, torch.uint8, torch.int8],
     }
 
     MACOS_BEFORE_13_3_XFAILLIST = {
@@ -402,7 +405,7 @@ def mps_ops_modifier(ops):
         'cdist': [torch.float32],
 
         # CPU Error: cpu not giving nan for x/0.0
-        'atan2': [torch.bool, torch.float16, torch.int16, torch.int32, torch.int64, torch.uint8, torch.int8],
+        'atan2': [torch.bool, torch.int16, torch.int32, torch.int64, torch.uint8, torch.int8],
 
         # test blow pass on macOS 12 as it falls back to cpu
         # Argsort case using duplicate indices (undefined behaviour):
@@ -689,6 +692,7 @@ def mps_ops_modifier(ops):
         # Unsupported dtypes
         'dot': [torch.int64],
         'index_add': [torch.int64],
+        'histc': [torch.float16],
         'log1p': [torch.int64],
         'sigmoid': [torch.int64],
         'atan2': [torch.int64],
@@ -779,6 +783,9 @@ def mps_ops_modifier(ops):
 
         # Failures due to casting negative float to uint8 is undefined
         'byte': [torch.float16, torch.float32],
+
+        # float output for float16 input on MPS
+        'logit': [torch.float16],
     }
 
     EMPTY_OPS_SKIPLIST = {
@@ -10810,7 +10817,8 @@ class TestConsistency(TestCaseMPS):
         'nn.functional.max_pool2d',
         'nn.functional.gelu',
         'nn.functional.glu',
-
+        'cross', 'linalg.cross',
+        'prod', 'masked.prod',
         # for macOS 12
         'masked.normalize', 'masked.sum', 'masked.var',
         'outer',
